059b826c495854a945101007a2b2bb1235729546
Ayman Omara <shadowadvs@hotmail.com>
2023-11-26 19:51:47 +0200

    customer front end

---
+++ resources/js/customers/services/API.js
@@ -0,0 +1,19 @@
+import {http} from '../vue-customers'
+
+class API {
+
+    constructor() {
+        this.http = http;
+    }
+
+    async getProducts(page) {
+        return await this.http.get(this.http.baseUrl + '/api/v1/products?page=' + page);
+    }
+
+    async orderCheckout(productsIds) {
+        return await this.http.post(this.http.baseUrl + '/api/v1/order-checkout', {products: productsIds});
+    }
+
+}
+
+export const APIService = new API();
