a6d2bad0bca7ced188b059f983c122dc1cc1a004
Ayman Omara <shadowadvs@hotmail.com>
2023-11-26 19:51:23 +0200

    init project

--- resources/views/customer-layout.blade.php
+++ resources/views/customer-layout.blade.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <link rel="preconnect" href="https://fonts.gstatic.com"/>
-    {{--{{ Html::style($css . '/en/bootstrap-en.css')}}--}}
-    {{--{{ Html::style($css . '/en/all-en.css?v=111')}}--}}
-{{--    <style src="{{asset('web/css/bootstrap-en.css}}" ></style>--}}
+    <link rel="stylesheet" href="{{asset('web/css/bootstrap-en.css') }}"/>
+    <link rel="stylesheet" href="{{asset('web/css/all-en.css') }}"/>
+    <link rel="stylesheet" href="{{asset('web/css/style.css') }}"/>
     <!--[if lt IE 9]>
     <script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -19,6 +19,9 @@
     <div id="app">
         <router-view></router-view>
     </div>
+    <script>
+        window.app_url = "{{request()->root()}}";
+    </script>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://kit.fontawesome.com/3706214aa1.js" crossorigin="anonymous"></script>
     <script src="{{asset('web/js/customers/vue-customers.js?v=111')}}"></script>
