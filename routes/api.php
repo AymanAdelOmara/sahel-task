5bfc4c6202efe67e6815482e6e2fdfb9ca812d5d
Ayman Omara <shadowadvs@hotmail.com>
2023-11-27 14:45:57 +0200

    admin controller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\OrdersController;
-use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/v1'], function () {
-    Route::post('order-checkout', [OrdersController::class, 'orderCheckout'])->name('orderCheckout');
-    Route::get('products', [ProductsController::class, 'index'])->name('getProducts');
+    Route::prefix('admin')->group(base_path('routes/api_admin.php'));
+    Route::prefix('customer')->group(base_path('routes/api_customer.php'));
 });
